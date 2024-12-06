3f69be41da8102139adec4298ff9cc83edc67528
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-06 11:08:19 +0900

    🔧 Update configureation files of code base

--- RxSwift_Tutorial/MainViewController.swift
+++ RxSwift_Tutorial/MainViewController.swift
@@ -1,19 +1,18 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RxSwift_Tutorial
 //
 //  Created by 전성규 on 12/6/24.
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+imp
 
-class ViewController: UIViewController {
-
+final class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
